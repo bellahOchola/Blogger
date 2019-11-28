60093380a09dcae5f2dca1053589fda327889eef
bellahOchola <bellahkenya@gmail.com>
2019-11-28 12:05:38 +0300

    add config file and import os

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+class Config:
+    class Config:
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+	UPLOADED_PHOTOS_DEST ='app/static/photos'
+	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:bellah@1972@localhost/pomodoro'
+
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+	MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+	@staticmethod
+	def init_app(app):
+		pass
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+	SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:bellah@1972@localhost/pomodoro'
+
+	DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
