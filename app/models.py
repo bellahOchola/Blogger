b18a6e89156dbc87822fd3805f01deed64d388ed
bellahOchola <bellahkenya@gmail.com>
2019-12-02 04:47:50 +0300

    add a new class subscriber in models file

--- app/models.py
+++ app/models.py
@@ -106,3 +106,16 @@ class Quotes:
         self.id = id
         self.author = author
         self.quote = quote
+
+class Subscriber(db.Model):
+    __tablename__='subscribers'
+
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Subscriber {self.email}'
